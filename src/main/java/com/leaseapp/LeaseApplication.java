4f5c4503d5a9d9d88fb3c80191ac330a5e1c5560
deepak <ddepakr@gmail.com>
2026-08-29 14:22:09 +0530

    evn file added

--- src/main/java/com/leaseapp/LeaseApplication.java
+++ src/main/java/com/leaseapp/LeaseApplication.java
@@ -1,10 +1,15 @@
 package com.leaseapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeaseApplication {
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LeaseApplication.class, args);
