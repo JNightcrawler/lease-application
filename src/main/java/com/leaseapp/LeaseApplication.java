5e68248e63a20672b2bd41786b251611069286e8
deepak <ddepakr@gmail.com>
2026-08-29 14:59:14 +0530

    dotenv removed

--- src/main/java/com/leaseapp/LeaseApplication.java
+++ src/main/java/com/leaseapp/LeaseApplication.java
@@ -1,15 +1,10 @@
 package com.leaseapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeaseApplication {
-
-    static {
-        Dotenv dotenv = Dotenv.load();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LeaseApplication.class, args);
